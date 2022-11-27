143ac794575dac5d39296c41378e74f0d4e5547c
Anastasiya B <gavrilovaanastasya@yandex.ru>
2022-11-27 12:39:09 +0300

    для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.

---
+++ HomeWork2.py
@@ -0,0 +1,12 @@
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = int(input('Введите число x: '))
+y = int(input('Введите число x: '))
+z = int(input('Введите число x: '))
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not(x, y or z) == (not x and not y and not z))
+            print(x, y, z)
+          
